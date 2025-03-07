8c3ad1589b3e766146dba30b9a06b8613be5cad4
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-03-07 05:00:49 +0400

    Create API endpoint for post and getting CartItem.

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewere
 app.use(cors());
 app.use(express.json());
+
+
+// add mongodb
+const uri = `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@cluster0.tkyyb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // create collection
+    const menuCollection = client.db('bistrobossDB').collection('menuCollection');
+    const cartCollection = client.db('bistrobossDB').collection('cartCollection');
+
+
+
+    // create all menu get API
+    app.get('/menu',async(req,res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.post('/carts',async(req,res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result);
+    })
+
+    app.get('/carts',async(req,res)=>{
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // get and lissen api
